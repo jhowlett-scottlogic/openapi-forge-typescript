72795cc2db1674092008c8769f05049177b8ada0
Colin Eberhardt <colin.eberhardt@gmail.com>
2022-06-29 09:39:59 +0100

    a bit of test refactoring

--- helpers/typeConvert.js
+++ helpers/typeConvert.js
@@ -1,4 +1,17 @@
-function typeConvert(prop) {
+function typeConvert(prop, options) {
+  // the feature tests involve dynamically creating generating multiple APIs, as a result
+  // the CommonJS 'require' style of module imports are used so that the cache can be invalidated.
+  // ES6 / ESM modules cannot be invalidated! In order to make this work with TypeScript, when using
+  // CommonJS, types need to be prefixed with 'typeof'
+  if (options && options.testRun) {
+    if (prop.$ref || (prop.type === "array" && prop.items.$ref)) {
+      return "typeof " + typeConverter(prop);
+    }
+  }
+  return typeConverter(prop);
+}
+
+function typeConverter(prop) {
   if (prop == undefined) return "";
 
   // resolve references
@@ -12,7 +25,7 @@ function typeConvert(prop) {
       return "number";
     case "string": {
       if (prop.format == "date-time" || prop.format == "date") {
-        return "Date"
+        return "Date";
       }
       return "string";
     }
@@ -23,7 +36,7 @@ function typeConvert(prop) {
     // inline object definition
     case "object":
       if (prop.additionalProperties) {
-        return `{ [name: string]: ${typeConvert(prop.additionalProperties)} }`
+        return `{ [name: string]: ${typeConvert(prop.additionalProperties)} }`;
       } else {
         return "{}";
       }
