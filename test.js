7700a08621b6b8af23b4c14881b3f176e0012e2c
jhowlett-scottlogic <107993905+jhowlett-scottlogic@users.noreply.github.com>
2022-09-23 13:09:45 +0100

    chore: Add testing to CI/CD (#12)
    * move shelljs from devDependencies to dependencies
    because test-generators command relies on it.
    Add const prefix to file paths in test.js
    
    * change slashes
    
    * bring in colin's changes
    
    * add logging
    
    * add logging
    
    * dwwd
    
    * add logging to featurePath
    
    * add quotes
    
    * commit
    
    * ded
    
    * wdwd
    
    * wd
    
    * wwd
    
    * wwdw
    
    * wd
    
    * wdw
    
    * wdwd
    
    * wdw
    
    * sdsd
    
    * dsdsd
    
    * wdd
    
    * eef
    
    * add continue-on-error
    
    * fix package.json

--- test.js
+++ test.js
@@ -4,10 +4,12 @@ const shell = require("shelljs");
 const clArgs = process.argv.slice(2);
 
 // Retrieve the path to feature paths from cl arguments of 'npm test', use default value if none given
-featurePath = clArgs[0] || "../openapi-forge/features/*.feature";
+const featurePath = clArgs[0] || "node_modules/openapi-forge/features/*.feature";
 
 // Retrieve the path to generate.js from cl arguments of 'npm test', use default value if none given
-generatePath = clArgs[1] || "openapi-forge/src/generate";
+const generatePath = clArgs[1] || "openapi-forge/src/generate";
 
 // Pass both paths to cucumber-js which spawns another node process to handle the testing.
-shell.exec(`.\\node_modules\\.bin\\cucumber-js -p default ${featurePath} ${generatePath}`);
+const result = shell.exec(`"./node_modules/.bin/cucumber-js" -p default "${featurePath}" "${generatePath}"`);
+
+process.exit(result.code);
